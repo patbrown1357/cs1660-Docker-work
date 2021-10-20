57cd8a1086e23f9a107fe4c5bef900652aecba8c
patbrown1357 <patbrown1357@gmail.com>
2021-10-19 20:56:25 -0400

    finished support'

--- Docker/homework_3/app.py
+++ Docker/homework_3/app.py
@@ -3,7 +3,12 @@ import csv
 
 def main(): 
 
- 
+    with open('keys.txt') as f:
+        lines = f.readlines()
+    print(lines[0])
+    print(lines[1])
+    aws_key = str.strip(lines[0])
+    aws_secret_key = str.strip(lines[1])
 
 
     s3 = boto3.resource('s3',
@@ -66,12 +71,40 @@ def main():
 
     print(table.item_count)
 
-    with open('ADD_A_PATH_TO_EXCEL', 'rb') as csvfile:
+    with open('..\spreadsheets\experiments.csv', 'rb') as csvfile:
         csvf = csv.reader(csvfile, delimeter=',', quotechar='|')
         for item in csvf:
             print(item)
-            body = open('LOCATION_OF_OTHER_EXP_FILES'+item[3], 'rb')
-            s3.Object('pob6-example-test', item[3]).put(Body=body)
+            body = open('..\spreadsheets\datafiles\\'+item[3], 'rb')
+            s3.Object('pob6-example-test2', item[3]).put(Body=body)
+
+            url = " https://s3-us-west-2.amazonaws.com/pob6-example-test/"+item[3]
+            metadata_item = { 'PartitionKey': item[0], 'RowKey': item[1], 
+                                'description': item[4], 'date': item[2], 'url':url }
+            try:
+                table.put_item(Item=metadata_item)
+            except:
+                    print("item may already be there or another failure")
+
+    ['experiment1', '1', '3/15/2002', 'exp1', 'this is the comment']
+    ['experiment1', '2', '3/15/2002', 'exp2', 'this is the comment2']
+    ['experiment1', '3', '3/16/2002', 'exp3', 'this is the comment3']
+    ['experiment1', '4', '3/16/2002', 'exp4', 'this is the comment4']
+
+    response = table.get_item(
+        Key={
+            'PartitionKey':'experiment3',
+            'RowKey':'4'
+        }
+    )
+    item = response['Item']
+    print(item)
+
+    {u'url': u'https://s3-us-west-2.amazonaws.com/pob6-example-test/exp4', 
+        u'date':u'3/16/2002', u'PartitionKey':u'experiment3', u'description':u'this is the comment233',
+        u'RowkKey': u'4'}
+
+    response
 
     return
 
