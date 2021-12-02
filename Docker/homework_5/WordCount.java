c6cc9c405781de04e063466f25e1d30da7cac1ea
Patrick Brown <pob6@pitt.edu>
2021-12-01 22:01:30 -0500

    new output

--- Docker/homework_5/WordCount.java
+++ Docker/homework_5/WordCount.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
+import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -24,7 +26,8 @@ public class WordCount {
         Job job = Job.getInstance(conf, "word count");
         job.setJarByClass(WordCount.class);
         job.setMapperClass(WordCountMapper.class);
-        job.setCombinerClass(WordCountReducer.class);
+        job.setMapOutputKeyClass(NullWritable.class);
+        job.setMapOutputValueClass(Text.class);
         job.setNumReduceTasks(1);
         job.setReducerClass(WordCountReducer.class);
         job.setOutputKeyClass(Text.class);
@@ -35,69 +38,73 @@ public class WordCount {
     }
 
     public static class WordCountMapper
-        extends Mapper<Object, Text, Text, IntWritable> {
+        extends Mapper<Object, Text, NullWritable, Text> {
 
-            public final List<String> stopWord = List.of("he", "she", "they", "the", "a", "an", "are", "you", "of", "is", "and", "or");
-            public final IntWritable zero = new IntWritable(0);
+            public final List<String> stopWord = Arrays.asList("he", "she", "they", "the", "a", "an", "are", "you", "of", "is", "and", "or");
             public final Text word = new Text();
-            public final TreeMap<Text, Integer> wordCount = new TreeMap<Text, Integer>();
-            public final TreeMap<Integer, Text> invert = new TreeMap<Integer, Text>();
+            public final HashMap<String, Integer> wordCount = new HashMap<String, Integer>();
+            public final TreeMap<Integer, String> topFive = new TreeMap<Integer, String>();
 
             public void map(Object key, Text value, Context context) 
                 throws IOException, InterruptedException {
                     StringTokenizer itr = new StringTokenizer(value.toString());
                     while(itr.hasMoreTokens()) {
-                        if(stopWord.contains(itr.nextToken())) {
+			            String token = itr.nextToken();
+                        if(stopWord.contains(token)) {
                             continue;
                         }
-                        word.set(itr.nextToken());
-                        if(wordCount.get(word) == null) {
-                            wordCount.put(word, 0);
+                        if(wordCount.get(token) == null) {
+                            wordCount.put(token, 0);
                         }
-                        wordCount.put(word, wordCount.get(word) + 1);
+                        wordCount.put(token, wordCount.get(token) + 1);
                     }
 
-                    for(Text word: wordCount.keySet()) {
-                        invert.put(wordCount.get(word), word);
-                    }
-                    int i = 0;
-                    for(Integer count: invert.descendingKeySet()) {
-                        if(i < 5) {
-                            context.write(NullWritable.get(), invert.get(count).append("-" + Integer.toString(count)));
-                            i++;
-                        } else {
-                            break;
+                    for(String s: wordCount.keySet()) {
+                        topFive.put(wordCount.get(s), s);
+                        if( topFive.size() > 5) {
+                            topFive.remove(topFive.firstKey());
                         }
                     }
             }
+
+            @Override
+            protected void cleanup(Context context) throws IOException, InterruptedException {
+               for(Map.Entry<Integer, String> entry: topFive.entrySet()) {
+                    context.write(NullWritable.get(), new Text(entry.getValue() + " " + entry.getKey()));
+               }
+            }
+
         }
 
         public static class WordCountReducer 
-            extends Reducer<Text, IntWritable, Text, IntWritable> {
+            extends Reducer<NullWritable, Text, Text, IntWritable> {
             
-            private Map rankings = new TreeMap<String, Integer>();
+            public final HashMap<String, Integer> rankings = new HashMap<String, Integer>();
             public void reduce(NullWritable key, Iterable<Text> values, Context context)
                     throws IOException, InterruptedException {
                 
                 for(Text value: values) {
-                    StringTokenizer itr = new StringTokenizer(value.toString(), "-");
+                    StringTokenizer itr = new StringTokenizer(value.toString());
                     String word = itr.nextToken();
                     int count = Integer.parseInt(itr.nextToken());
                     if(rankings.get(word) == null) {
                         rankings.put(word, 0);
                     }
-                    rankings.put(word, count + rankings.get(word));
+		            int val = (int)rankings.get(word);
+                    rankings.put(word, count + val);
                 }
 
                 TreeMap<Integer, String> invert = new TreeMap<Integer, String>();
-                for(Text word: rankings.keySet()) {
-                    invert.put(wordCount.get(word), word);
+                for(Object word: rankings.keySet()) {
+		            String w2 = word.toString();
+                    invert.put(rankings.get(w2), w2);
                 }
 
                 int i = 0;
-                for(Integer count: rankings.descendingKeySet()) {
+                for(Object count: invert.descendingKeySet()) {
                     if(i < 5) {
-                        context.write(invert.get(count), new IntWritable(count));
+			            int c2 = (int) count;
+                        context.write(new Text(invert.get(c2)), new IntWritable(c2));
                         i++;
                     } else {
                         break;
